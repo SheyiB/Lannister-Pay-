51d1329109312ba1bc196b7b86a1fc4c1c5ace84
Elijah Oluwaseyifunmi Banjo <elijahbanjo@gmail.com>
2022-03-21 19:10:31 +0100

    update

--- index.js
+++ index.js
@@ -235,7 +235,7 @@ app.post('/compute-transaction-fee', async (req, res) =>  {
     }
 
 })
-const PORT = 4000;
+const PORT = process.env.PORT || 4000;
 
 app.listen(PORT, () => {
     console.log(`App listening on port: ${4000}!`.bgGreen.black);
